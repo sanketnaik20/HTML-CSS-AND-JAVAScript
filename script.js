e09b85bf7415b5ad7bea3d5f5f317ffea9ae5474
sanketnaik20 <sanketnaik2004@gmail.com>
2023-10-20 22:21:44 +0530

    added mouse follower

--- script.js
+++ script.js
@@ -29,5 +29,30 @@ gsap.from(".nav button  ", {
 
 })
 
+let posX = 0,
+    posY = 0;
 
+let mouseX = 0,
+    mouseY = 0;
+
+gsap.to(".cursor-example", {
+    duration: 0.0085,
+    repeat: -1,
+    onRepeat: function () {
+        posX += (mouseX - posX) / 8;
+        posY += (mouseY - posY) / 8;
+
+        gsap.set(".cursor-example", {
+            css: {
+                left: posX - 1,
+                top: posY - 2
+            }
+        });
+    }
+});
+
+document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
 
